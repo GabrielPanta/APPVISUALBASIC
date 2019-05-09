208d6ab68db2911adbce0d0088ca1ec48bfd7794
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-05-08 23:47:30 -0500

    Crud (registrar,mostrar)

---
+++ APPVISUALBASIC/APPVISUALBASIC/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtcodigo = New System.Windows.Forms.TextBox()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtapellidos = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnmostrardatos = New System.Windows.Forms.Button()
+        Me.btneliminar = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.Datos = New System.Windows.Forms.DataGridView()
+        Me.btnguardar = New System.Windows.Forms.Button()
+        CType(Me.Datos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtcodigo
+        '
+        Me.txtcodigo.Location = New System.Drawing.Point(100, 39)
+        Me.txtcodigo.Name = "txtcodigo"
+        Me.txtcodigo.Size = New System.Drawing.Size(84, 20)
+        Me.txtcodigo.TabIndex = 0
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(100, 65)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(133, 20)
+        Me.txtnombre.TabIndex = 1
+        '
+        'txtapellidos
+        '
+        Me.txtapellidos.Location = New System.Drawing.Point(100, 91)
+        Me.txtapellidos.Name = "txtapellidos"
+        Me.txtapellidos.Size = New System.Drawing.Size(214, 20)
+        Me.txtapellidos.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Codigo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 98)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Apellidos"
+        '
+        'btnmostrardatos
+        '
+        Me.btnmostrardatos.Location = New System.Drawing.Point(29, 119)
+        Me.btnmostrardatos.Name = "btnmostrardatos"
+        Me.btnmostrardatos.Size = New System.Drawing.Size(103, 23)
+        Me.btnmostrardatos.TabIndex = 6
+        Me.btnmostrardatos.Text = "Mostrar Datos"
+        Me.btnmostrardatos.UseVisualStyleBackColor = True
+        '
+        'btneliminar
+        '
+        Me.btneliminar.Location = New System.Drawing.Point(320, 63)
+        Me.btneliminar.Name = "btneliminar"
+        Me.btneliminar.Size = New System.Drawing.Size(103, 23)
+        Me.btneliminar.TabIndex = 7
+        Me.btneliminar.Text = "Eliminar"
+        Me.btneliminar.UseVisualStyleBackColor = True
+        '
+        'btnsalir
+        '
+        Me.btnsalir.Location = New System.Drawing.Point(320, 91)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(103, 23)
+        Me.btnsalir.TabIndex = 8
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = True
+        '
+        'Datos
+        '
+        Me.Datos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Datos.Location = New System.Drawing.Point(29, 148)
+        Me.Datos.Name = "Datos"
+        Me.Datos.Size = New System.Drawing.Size(394, 117)
+        Me.Datos.TabIndex = 9
+        '
+        'btnguardar
+        '
+        Me.btnguardar.Location = New System.Drawing.Point(320, 34)
+        Me.btnguardar.Name = "btnguardar"
+        Me.btnguardar.Size = New System.Drawing.Size(103, 23)
+        Me.btnguardar.TabIndex = 10
+        Me.btnguardar.Text = "Guardar"
+        Me.btnguardar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 317)
+        Me.Controls.Add(Me.btnguardar)
+        Me.Controls.Add(Me.Datos)
+        Me.Controls.Add(Me.btnsalir)
+        Me.Controls.Add(Me.btneliminar)
+        Me.Controls.Add(Me.btnmostrardatos)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtapellidos)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.txtcodigo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Datos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtcodigo As TextBox
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtapellidos As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnmostrardatos As Button
+    Friend WithEvents btneliminar As Button
+    Friend WithEvents btnsalir As Button
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Friend WithEvents Datos As DataGridView
+    Friend WithEvents btnguardar As Button
+End Class
---
+++ APPVISUALBASIC/APPVISUALBASIC/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conex As New MySqlConnection("
+                                    data source=localhost;
+                                    user id=root;
+                                    password='';
+                                    database=persona")
+    Dim da As MySqlDataAdapter
+    Dim dt As DataTable
+    Dim sql As String
+    Dim comando As MySqlCommand
+
+    Private Sub btnmostrardatos_Click(sender As Object, e As EventArgs) Handles btnmostrardatos.Click
+
+        Call mostrardatos()
+
+    End Sub
+
+    Private Sub mostrardatos()
+        Try
+            sql = "select * from datos"
+            da = New MySqlDataAdapter(sql, conex)
+            dt = New DataTable
+            da.Fill(dt)
+            Datos.DataSource = dt
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Datos_RowEnter(sender As Object, e As DataGridViewCellEventArgs) Handles Datos.RowEnter
+        sql = Datos.Rows(e.RowIndex).Cells(0).Value.ToString
+    End Sub
+
+    Private Sub btnguardar_Click(sender As Object, e As EventArgs) Handles btnguardar.Click
+        Call guardardatos()
+    End Sub
+
+    Private Sub guardardatos()
+        Dim r As Integer
+        Try
+            conex.Open()
+            comando = New MySqlCommand("insert into datos values('" & txtcodigo.Text & "','" & txtnombre.Text & "','" & txtapellidos.Text & "')", conex)
+            r = comando.ExecuteNonQuery()
+            If r > 0 Then
+                MsgBox("Registrado Correctamente")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+        conex.Close()
+    End Sub
+
+End Class
